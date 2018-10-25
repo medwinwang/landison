4aa590cbbdcaefc400532554eb846b9a03203d8f
medwin <wm1990315@gmail.com>
2018-10-25 11:52:58 +0800

    请求使用连接池

--- src/main/java/com/medwin/landison/config/RestClientConfig.java
+++ src/main/java/com/medwin/landison/config/RestClientConfig.java
@@ -1,17 +1,28 @@
 package com.medwin.landison.config;
 
+import org.apache.http.Header;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by medwin on 2018/10/16.
@@ -26,7 +37,44 @@ public class RestClientConfig {
     // 声明为Bean，方便应用内使用同一实例
     @Bean
     public RestTemplate restTemplate() {
+
+        // 长连接保持30秒
+        PoolingHttpClientConnectionManager pollingConnectionManager =
+                new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
+        // 总连接数
+        pollingConnectionManager.setMaxTotal(1000);
+        // 同路由的并发数
+        pollingConnectionManager.setDefaultMaxPerRoute(1000);
+
+        HttpClientBuilder httpClientBuilder = HttpClients.custom();
+        httpClientBuilder.setConnectionManager(pollingConnectionManager);
+        // 重试次数，默认是3次，没有开启
+        httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(2, true));
+        // 保持长连接配置，需要在头添加Keep-Alive
+        httpClientBuilder.setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy());
+
+        List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36"));
+        headers.add(new BasicHeader("Accept-Encoding", "gzip,deflate"));
+        headers.add(new BasicHeader("Accept-Language", "zh-CN"));
+        headers.add(new BasicHeader("Connection", "Keep-Alive"));
+
+        httpClientBuilder.setDefaultHeaders(headers);
+
+        HttpClient httpClient = httpClientBuilder.build();
+
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        // 连接超时
+        clientHttpRequestFactory.setConnectTimeout(5000);
+        // 数据读取超时时间，即SocketTimeout
+        clientHttpRequestFactory.setReadTimeout(5000);
+        // 连接不够用的等待时间，不宜过长，必须设置，比如连接不够用时，时间过长将是灾难性的
+        clientHttpRequestFactory.setConnectionRequestTimeout(200);
+        // 缓冲请求数据，默认值是true。通过POST或者PUT大量发送数据时，建议将此属性更改为false，以免耗尽内存。
+         clientHttpRequestFactory.setBufferRequestBody(false);
+
         RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(clientHttpRequestFactory);
 
         // 把自定义的ClientHttpRequestInterceptor添加到RestTemplate，可添加多个
         restTemplate.setInterceptors(Collections.singletonList(actionTrackInterceptor));
